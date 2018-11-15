763a1e14f13b30c1fd567f3f8343f859b5569c13
Mikkel Dalby Nielsen <mikkel@dalbynielsen.dk>
2018-11-15 13:47:11 +0100

    Initial commit

---
+++ src/main/java/dk/webshopmodule/WebshopmoduleApplication.java
@@ -0,0 +1,12 @@
+package dk.webshopmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopmoduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebshopmoduleApplication.class, args);
+    }
+}
---
+++ src/test/java/dk/webshopmodule/WebshopmoduleApplicationTests.java
@@ -0,0 +1,16 @@
+package dk.webshopmodule;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WebshopmoduleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
